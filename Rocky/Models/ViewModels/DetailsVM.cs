ca6e04110de0f8407f85f02432f4d9973e28c3a7
zarar384 <fel98_98@mail.ru>
2022-06-18 00:28:31 +0200

    Add Home Page

---
+++ Rocky/Models/ViewModels/DetailsVM.cs
@@ -0,0 +1,10 @@
+namespace Rocky.Models.ViewModels{
+    public class DetailsVM{
+        public DetailsVM()
+        {
+            Product = new Product();
+        }
+        public Product Product { get; set; }
+        public bool ExistsInCart { get; set; }
+    }
+}
